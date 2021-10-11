7c3672edf9e4e70c08acbfc8bf92ee2cc61f2cd4
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-10-11 22:40:17 +0700

    fsdfs

---
+++ js/main.js
@@ -0,0 +1,107 @@
+const $=document.querySelector.bind(document)
+const $$=document.querySelectorAll.bind(document)
+
+
+const firstNumberElement=$('#first-number')
+const secondNumberElement=$('#second-number')
+const resultElement=$('#result')
+const btnCal=$('.btn-calculator')
+var type
+
+
+
+var firstValue;
+var secondValue;
+var checkClick1=true
+var checkClick2=true
+
+
+resultElement.addEventListener('keypress',(e)=>{
+    e.preventDefault()
+    return false
+})
+
+const validatorNumber=(string)=>{
+    if(/[^\.\w]/.test(string)){
+        return false
+    }
+    if(/[a-z]/.test(string)){
+        return false
+    }
+    return true
+}
+firstNumberElement.addEventListener('input',(e)=>{
+    $('#success').innerText=""
+    resultElement.value=""
+    let index=e.target.value.indexOf('.')
+    if(index>0){
+        firstValue= parseFloat(e.target.value)
+    }
+    else{
+        firstValue= parseInt(e.target.value)
+    }
+    if (validatorNumber(e.target.value)){
+        if(!isNaN(firstValue)){
+            console.log(firstValue)
+            $('#error1').innerText=''
+        }
+        checkClick1=true
+    }
+    else{
+        checkClick1=false
+        $('#error1').innerText="Giá trị ô thứ nhất không phải là số"
+    }
+    
+})
+secondNumberElement.addEventListener('input',(e)=>{
+    resultElement.value=""
+    $('#success').innerText=""
+    let index=e.target.value.indexOf('.')
+    if(index>0){
+        secondValue= parseFloat(e.target.value)
+    }
+    else{
+        secondValue= parseInt(e.target.value)
+    }
+    if(validatorNumber(e.target.value)){
+        if(!isNaN(secondValue)){
+            console.log(secondValue)
+            $('#error2').innerText=''
+        }
+        checkClick2=true
+    }
+    else{
+        $('#error2').innerText="Giá trị ô thứ hai không phải là số"
+        checkClick2=false
+        
+    }
+})
+
+btnCal.addEventListener('click',(e)=>{
+    if(checkClick1&&checkClick2){
+        type=$('input[name=choseone]:checked').value
+        if(!isNaN(secondValue)&&(!isNaN(firstValue))){
+        if(type==="add"){
+            result=secondValue+firstValue
+        }
+        else if (type==="sub"){
+            result=firstValue-secondValue
+        }
+        else if (type==="div"){
+            result=(firstValue/secondValue).toFixed(3)
+        }
+        else if(type==="mul"){
+            result=firstValue*secondValue
+        }
+        resultElement.value=result
+        $('#success').innerText="Successful!!"
+        return 
+        }
+    }
+})
+setInterval(()=>{
+    if($('input[name=choseone]:checked').value!==type){
+        $('#success').innerText=''
+        resultElement.value=''
+    }
+},200)
